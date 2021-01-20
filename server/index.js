af9ebd5e34251c54d6c4dd34ae6b38e05eefb56b
Dima Belimenko <idbelimenko@gmail.com>
2021-01-21 01:49:31 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const port = 5000;
 
 app.use(cors());
 app.use(express.json());
-
+io.on('connection', socket => {
+  socket.on('message', ({ name, message }) => {
+    io.emit('message', { name, message })
+  })
+})
 
 http.listen(port, function() {
   console.log('listening on port 5000')
